e2837eac12a38655670af105cbdfd61751f2c4f3
mal031100 <88300591+mal031100@users.noreply.github.com>
2021-12-06 09:10:02 +0700

    update

--- app/Models/CategoryBrand.php
+++ app/Models/CategoryBrand.php
@@ -15,7 +15,7 @@ class CategoryBrand extends Model
         'brand_id',
     ];
     
-    public function categoey(){
+    public function category(){
         return $this->belongsTo(Category::class, 'category_id', 'id');
     }
 
