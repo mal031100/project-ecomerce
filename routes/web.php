151d6654568c2e92ba771578f861bc2c92b8c0eb
mal031100 <88300591+mal031100@users.noreply.github.com>
2021-12-17 14:36:47 +0700

    Login

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix'=>'', 'as'=>'account.'], function(){
+    Route::get('', ['name'=>'formlogin', 'uses'=>'Auth\AuthController@formlogin'])->name('formlogin');
+    Route::post('login', ['name'=>'login', 'uses'=>'Auth\AuthController@login'])->name('login');
+    Route::get('register', ['name'=>'formregister', 'uses'=>'Auth\AuthController@formregister'])->name('formregister');
+    Route::post('register', ['name'=>'register', 'uses'=>'Auth\AuthController@register'])->name('register');
+    Route::get('logout', ['name'=>'logout', 'uses'=>'Auth\AuthController@logout'])->name('logout');
 
-// Route::prefix('amin')->group(function(){
-//     Route::get('/', [HomeController::class, 'index'])->name('admin.index');
-// });
-
-// Route::prefix('client')->group(function(){
-//     Route::get('/', [ClientController::class, 'index'])->name('client.index');
-// });
+    Route::get('list', ['name'=>'list', 'uses'=>'Auth\AuthController@list'])->name('list');
+});
 
 Route::group(['prefix'=>'amin', 'as'=>'admin.'], function(){
-    Route::get('/', ['as'=>'index', 'uses' => 'Admin\HomeController@index']);
+    Route::get('/', ['name'=>'index', 'uses' => 'Admin\HomeController@index'])->name('index');
     Route::group(['prefix'=>'product', 'as'=>'product.'], function(){
         Route::get('',['name'=>'list', 'uses'=>'Admin\ProductController@list'])->name('list');
         Route::get('add-product',['name'=>'add', 'uses'=>'Admin\ProductController@add'])->name('add');
