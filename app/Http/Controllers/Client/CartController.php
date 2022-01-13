c5effaaa666ffca4c4eaa66a2e9501768807d8d2
mal031100 <88300591+mal031100@users.noreply.github.com>
2022-01-13 09:17:56 +0700

    add view

--- app/Http/Controllers/Client/CartController.php
+++ app/Http/Controllers/Client/CartController.php
@@ -2,11 +2,43 @@
 
 namespace App\Http\Controllers\Client;
 
+use Gloudemans\Shoppingcart\Cart;
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Session;
 
 class CartController extends Controller
 {
+    public function save_cart(Request $request, $id)
+    {   
+        $total = 0;
+        // $id = $request->input('id');
+        // $quantity = $request->input('amount');
+        // $getpro = Product::find($id);
+        // $name = $getpro->name;
+        // $image = $getpro->image;
+        // $price = $getpro->sale;
+        $product = Product::find($id);
+        $name = $product->name;
+        $image = $product->image;
+        $quantity = $product->amount;
+        $price = $product->sale;
+
+        $data = [
+            'id' => $id,
+            'qty' => $quantity,
+            'name' => $name,
+            'price' => $price,
+            'options' => [
+                'image' => $image,
+            ],
+            'total' => [$total + ($price * $quantity)],
+        ];
+        Cart::add($data);
+
+        return redirect()->back();
+    }
     public function cart(){
         return view('client.cart.cart');
     }
--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,7 @@ Route::group(['prefix'=>'amin','as'=>'admin.'], function(){
 
 Route::group(['prefix'=>'client', 'as'=>'client.'], function(){
     Route::get('/', ['as'=>'index', 'uses' => 'Client\ClientController@index']);
+    Route::get('detail/{id}', ['as'=>'detail', 'uses'=>'Client\DetailController@index']);
+    Route::post('add-cart', ['as'=>'addCart', 'uses'=>'Client\CartController@save_cart']);
     Route::get('cart', ['as'=>'cart', 'uses'=>'Client\CartController@cart']);
-    Route::get('detail', ['as'=>'detail', 'uses'=>'Client\DetailController@index']);
 });
